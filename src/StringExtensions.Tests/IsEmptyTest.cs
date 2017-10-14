f110d296b5ba77fa24047fd941243e6c2032e2de
Kohei Hakoishi <wipiano@gmail.com>
2017-10-14 22:46:00 +0900

    add IsEmpty, IsNull, IsNullOrEmpty, IsNullOrWhiteSpace

---
+++ src/StringExtensions.Tests/IsEmptyTest.cs
@@ -0,0 +1,31 @@
+﻿using Xunit;
+
+namespace StringExtensions.Tests
+{
+    public class IsEmptyTest
+    {
+        [Xunit.Fact]
+        public void IsEmptyTests()
+        {
+            (null as string).IsNull().IsTrue();
+            "".IsNull().IsFalse();
+            "hoge".IsNull().IsFalse();
+            " ".IsNull().IsFalse();
+            
+            (null as string).IsEmpty().IsFalse();
+            "".IsEmpty().IsTrue();
+            "hoge".IsEmpty().IsFalse();
+            " ".IsEmpty().IsFalse();
+            
+            (null as string).IsNullOrEmpty().IsTrue();
+            "".IsNullOrEmpty().IsTrue();
+            "hoge".IsNullOrEmpty().IsFalse();
+            " ".IsNullOrEmpty().IsFalse();
+
+            (null as string).IsNullOrWhiteSpace().IsTrue();
+            "".IsNullOrWhiteSpace().IsTrue();
+            "hoge".IsNullOrWhiteSpace().IsFalse();
+            " ".IsNullOrWhiteSpace().IsTrue();
+        }
+    }
+}
---
+++ src/StringExtensions/IsEmpty.cs
@@ -0,0 +1,37 @@
+﻿namespace StringExtensions
+{
+    public static partial class StringEx
+    {
+        /// <summary>
+        /// Indicates whether the specified string is null or an Empty string
+        /// </summary>
+        /// <param name="s"></param>
+        /// <returns></returns>
+        public static bool IsNullOrEmpty(this string s)
+            => string.IsNullOrEmpty(s);
+
+        /// <summary>
+        /// Indicates whether a specified string is null, empty, or consists only of white-space characters.
+        /// </summary>
+        /// <param name="s"></param>
+        /// <returns></returns>
+        public static bool IsNullOrWhiteSpace(this string s)
+            => string.IsNullOrWhiteSpace(s);
+
+        /// <summary>
+        /// Indicates whether the specified string is null
+        /// </summary>
+        /// <param name="s"></param>
+        /// <returns></returns>
+        public static bool IsNull(this string s)
+            => s == null;
+
+        /// <summary>
+        /// Indicates whether the specified string is an Empty string
+        /// </summary>
+        /// <param name="s"></param>
+        /// <returns></returns>
+        public static bool IsEmpty(this string s)
+            => s == string.Empty;
+    }
+}
