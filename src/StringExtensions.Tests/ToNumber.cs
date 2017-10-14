c57b1fb18be3940599dcc606f4e7dbb36ec7cf02
Kohei Hakoishi <wipiano@gmail.com>
2017-10-14 23:29:31 +0900

    ToNumber

---
+++ src/StringExtensions.Tests/ToNumber.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Xunit;
+
+namespace StringExtensions.Tests
+{
+    public class ToNumber
+    {
+        [Fact]
+        public void ToNumberTest()
+        {
+            "-123".ToSignedByte().Is((sbyte)-123);
+            "234".ToByte().Is((byte)234);
+            "-23456".ToShort().Is((short)-23456);
+            "65432".ToUnsignedShort().Is((ushort)65432);
+            "-123456".ToInt().Is(-123456);
+            "123456".ToUnsignedInt().Is(123456u);
+            "-123456".ToLong().Is(-123456L);
+            "123456".ToUnsignedLong().Is(123456uL);
+            
+            // failed to parse
+            Assert.Throws<ArgumentNullException>(() => (null as string).ToInt());
+            Assert.Throws<FormatException>(() => "hoge".ToInt());
+            Assert.Throws<OverflowException>(() => "12345".ToByte());
+            
+            // failed to parse
+            
+        }
+    }
+}
---
+++ src/StringExtensions/ToNumber.cs
@@ -0,0 +1,71 @@
+﻿namespace StringExtensions
+{
+    public static partial class StringEx
+    {
+        /// <summary>
+        /// parse string as sbyte
+        /// </summary>
+        public static sbyte ToSignedByte(this string s)
+            => sbyte.Parse(s);
+
+        /// <summary>
+        /// parse string as byte
+        /// </summary>
+        public static byte ToByte(this string s)
+            => byte.Parse(s);
+
+        /// <summary>
+        /// parse string as short
+        /// </summary>
+        public static short ToShort(this string s)
+            => short.Parse(s);
+
+        /// <summary>
+        /// parse string as ushort
+        /// </summary>
+        public static ushort ToUnsignedShort(this string s)
+            => ushort.Parse(s);
+
+        /// <summary>
+        /// parse string as int
+        /// </summary>
+        public static int ToInt(this string s)
+            => int.Parse(s);
+
+        /// <summary>
+        /// parse string as uint
+        /// </summary>
+        public static uint ToUnsignedInt(this string s)
+            => uint.Parse(s);
+
+        /// <summary>
+        /// parse string as long
+        /// </summary>
+        public static long ToLong(this string s)
+            => long.Parse(s);
+
+        /// <summary>
+        /// parse string as ulong
+        /// </summary>
+        public static ulong ToUnsignedLong(this string s)
+            => ulong.Parse(s);
+
+        /// <summary>
+        /// parse string as float
+        /// </summary>
+        public static float ToFloat(this string s)
+            => float.Parse(s);
+
+        /// <summary>
+        /// parse string as double
+        /// </summary>
+        public static double ToDouble(this string s)
+            => double.Parse(s);
+
+        /// <summary>
+        /// parse string as decimal
+        /// </summary>
+        public static decimal ToDecimal(this string s)
+            => decimal.Parse(s);
+    }
+}
