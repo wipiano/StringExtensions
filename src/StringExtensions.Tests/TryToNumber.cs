af7ad42781719e2b0af0419df82b33a70ce95c58
Kohei Hakoishi <wipiano@gmail.com>
2017-10-14 23:55:43 +0900

    add ToX (tryparse)

---
+++ src/StringExtensions.Tests/TryToNumber.cs
@@ -0,0 +1,79 @@
+﻿using Xunit;
+
+namespace StringExtensions.Tests
+{
+    public class TryToNumber
+    {
+        [Fact]
+        public void TryToNumberTest()
+        {
+            {
+                "-123".ToSignedByte(out var result).IsTrue();
+                result.Is((sbyte)-123);
+            }
+
+            {
+                "234".ToByte(out var result).IsTrue();
+                result.Is((byte)234);
+            }
+
+            {
+                "-23456".ToShort(out var result).IsTrue();
+                result.Is((short)-23456);
+            }
+
+            {
+                "65432".ToUnsignedShort(out var result).IsTrue();
+                result.Is((ushort)65432);
+            }
+
+            {
+                "-123456".ToInt(out var result).IsTrue();
+                result.Is(-123456);
+            }
+
+            {
+                "123456".ToUnsignedInt(out var result).IsTrue();
+                result.Is(123456u);
+            }
+
+            {
+                "-123456".ToLong(out var result).IsTrue();
+                result.Is(-123456L);
+            }
+
+            {
+                "123456".ToUnsignedLong(out var result).IsTrue();
+                result.Is(123456uL);
+            }
+
+            {
+                "123.456".ToFloat(out var result).IsTrue();
+                result.Is(123.456f);
+            }
+
+            {
+                "123.4567890".ToDouble(out var result).IsTrue();
+                result.Is(123.4567890);
+            }
+
+            {
+                "1948385.48484848".ToDecimal(out var result).IsTrue();
+                result.Is(1948385.48484848m);
+            }
+
+            // failed to parse
+            {
+                (null as string).ToInt(out var result).IsFalse();
+            }
+
+            {
+                "hoge".ToInt(out var result).IsFalse();
+            }
+
+            {
+                "1234567890".ToByte(out var result).IsFalse();
+            }
+        }
+    }
+}
---
+++ src/StringExtensions/TryToNumber.cs
@@ -0,0 +1,71 @@
+﻿namespace StringExtensions
+{
+    public static partial class StringEx
+    {
+        /// <summary>
+        /// try parse string as sbyte
+        /// </summary>
+        public static bool ToSignedByte(this string s, out sbyte result)
+            => sbyte.TryParse(s, out result);
+
+        /// <summary>
+        /// try parse string as byte
+        /// </summary>
+        public static bool ToByte(this string s, out byte result)
+            => byte.TryParse(s, out result);
+
+        /// <summary>
+        /// try parse string as short
+        /// </summary>
+        public static bool ToShort(this string s, out short result)
+            => short.TryParse(s, out result);
+
+        /// <summary>
+        /// try parse string as ushort
+        /// </summary>
+        public static bool ToUnsignedShort(this string s, out ushort result)
+            => ushort.TryParse(s, out result);
+
+        /// <summary>
+        /// try parse string as int
+        /// </summary>
+        public static bool ToInt(this string s, out int result)
+            => int.TryParse(s, out result);
+
+        /// <summary>
+        /// try parse string as uint
+        /// </summary>
+        public static bool ToUnsignedInt(this string s, out uint result)
+            => uint.TryParse(s, out result);
+
+        /// <summary>
+        /// try parse string as long
+        /// </summary>
+        public static bool ToLong(this string s, out long result)
+            => long.TryParse(s, out result);
+
+        /// <summary>
+        /// try parse string as ulong
+        /// </summary>
+        public static bool ToUnsignedLong(this string s, out ulong result)
+            => ulong.TryParse(s, out result);
+
+        /// <summary>
+        /// try parse string as float
+        /// </summary>
+        public static bool ToFloat(this string s, out float result)
+            => float.TryParse(s, out result);
+
+        /// <summary>
+        /// try parse string as double
+        /// </summary>
+        public static bool ToDouble(this string s, out double result)
+            => double.TryParse(s, out result);
+
+        /// <summary>
+        /// try parse string as decimal
+        /// </summary>
+        public static bool ToDecimal(this string s, out decimal result)
+            => decimal.TryParse(s, out result);
+    }
+}
